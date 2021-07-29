31bcd1ec57ad31d353a469a57c93e45bccd1ac97
tpal-dev <tpal.developer@gmail.com>
2021-07-29 21:37:11 +0200

    moved service method to helper class

--- lib/services/api_path.dart
+++ lib/services/api_path.dart
@@ -1,7 +1,5 @@
 import 'package:flutter/foundation.dart';
 
-/// job path '/users/$uid/jobs/$jobId'
-
 class APIPath {
   static String job({@required String uid, @required String jobId}) => 'users/$uid/jobs/$jobId';
   static String jobs({@required String uid}) => 'users/$uid/jobs';
--- lib/services/database.dart
+++ lib/services/database.dart
@@ -1,7 +1,7 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
 import 'package:time_tracker_app/app/home/models/job.dart';
 import 'package:time_tracker_app/services/api_path.dart';
+import 'package:time_tracker_app/services/firestore_service.dart';
 
 abstract class Database {
   Future<void> createJob(Job job);
@@ -13,27 +13,12 @@ class FirestoreDatabase implements Database {
 
   final String uid;
 
-  Stream<List<T>> _collectionStream<T>(
-      {@required String path, @required T Function(Map<String, dynamic> data) builder}) {
-    final reference = FirebaseFirestore.instance.collection(path);
-    final snapshots = reference.snapshots();
-    return snapshots.map((snapshot) => snapshot.docs
-        .map(
-          (snapshot) => builder(snapshot.data()),
-        )
-        .toList());
-  }
-
-  Future<void> _setData({String path, Map<String, dynamic> data}) async {
-    final reference = FirebaseFirestore.instance.doc(path);
-    print('$path: $data');
-    await reference.set(data);
-  }
+  final _service = FirestoreService.instance;
 
   Stream<List<Job>> jobStream() =>
-      _collectionStream(path: APIPath.jobs(uid: uid), builder: (data) => Job.fromMap(data));
+      _service.collectionStream(path: APIPath.jobs(uid: uid), builder: (data) => Job.fromMap(data));
 
-  Future<void> createJob(Job job) async => _setData(
+  Future<void> createJob(Job job) async => _service.setData(
         path: APIPath.job(uid: uid, jobId: 'job_abc'),
         data: job.toMap(),
       );
