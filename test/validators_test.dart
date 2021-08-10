52764a27639f63f5f9b8f25fcf5dad47afd26332
tpal-dev <tpal.developer@gmail.com>
2021-08-10 11:20:38 +0200

    unit test validators

---
+++ test/validators_test.dart
@@ -0,0 +1,17 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:time_tracker_app/app/sign_in/validators.dart';
+
+void main() {
+  test('non empty string', () {
+    final validator = NonEmptyStringValidator();
+    expect(validator.isValid('value'), true);
+  });
+  test('empty string', () {
+    final validator = NonEmptyStringValidator();
+    expect(validator.isValid(''), false);
+  });
+  test('null string', () {
+    final validator = NonEmptyStringValidator();
+    expect(validator.isValid(null), false);
+  });
+}
